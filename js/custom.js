b4e5e95074098ccec709401b93d9128980d8eae8
Dan Michael O. Heggø <d.m.heggo@ub.uio.no>
2017-12-05 16:24:38 +0100

    Add fixed footer

--- js/custom.js
+++ js/custom.js
@@ -13,20 +13,64 @@
 
     /****************************************************************************************************/
 
-	
-	// Add Clickable Logo
-    app.component('prmLogoAfter', {
-        bindings: {parentCtrl: '<'},
-        controller: 'prmLogoAfterController',
-        template: '<div class="product-logo product-logo-local" layout="row" layout-align="start center" layout-fill id="banner"><a href="https://bibsys-almaprimo.hosted.exlibrisgroup.com/primo-explore/search?vid=UBO&lang=no_NO"><img class="logo-image" alt="{{::(\'nui.header.LogoAlt\' | translate)}}" ng-src="{{$ctrl.getIconLink()}}"/></a></div>'
-    }); 
 
-	app.controller('prmLogoAfterController', [function () {
+    app.component('prmSearchAfter', {
+        bindings: {parentCtrl: '<'},
+        controller: 'prmSearchAfterController',
+        template: `<div class="footer">
+
+            <div layout="row" layout-align="center start" flex>
+
+                <div flex="0" flex-md="0" flex-lg="10" flex-xl="20" ng-class="{'flex-lgPlus-15': $ctrl.parentCtrl.mediaQueries.lgPlus && !$ctrl.parentCtrl.facetToLeft, 'flex-lgPlus-20': $ctrl.parentCtrl.mediaQueries.lgPlus && $ctrl.parentCtrl.facetToLeft, 'flex-xl-25': $ctrl.parentCtrl.facetToLeft}"></div>
+
+                <div flex style="padding: 16px 20px; display: flex;">
+
+                    <div style="flex: 0 0 auto;" class="logo">
+                        <a href="https://www.ub.uio.no" title="Universitetsbiblioteket i Oslo" class="md-primoExplore-theme" style="display: block; margin-right: 1em; padding: 0;">
+                            <img src="custom/UBO/img/UiO_Segl.png" alt=""></a>
+                    </div>
+                    <div style="flex: 1 0 auto;">
+                        <!-- spacing -->
+                    </div>
+                    <div style="flex: 0 0 auto; padding: 0 1em;" class="uio-mobile">
+                        <div>
+                            <a href="#">oria-ub@ub.uio.no</a>
+                            <a href="https://www.ub.uio.no/om/personvernerkleringer/">Personvernerklæring</a>
+                        </div>
+                    </div>
+                    <div style="flex: 0 0 auto; padding: 0 1em;" class="uio-contact">
+                        <div class="md-headline">Kontaktinformasjon</div>
+                        <div>
+                            <a href="#">oria-ub@ub.uio.no</a>
+                            <a href="#">Kontakt oss</a>
+                        </div>
+                    </div>
+                    <div style="flex: 0 0 auto; padding: 0 1em;" class="uio-responsible">
+                        <div class="md-headline">Ansvarlig for denne siden</div>
+                        <div>
+                            <a href="#">UiO : Universitetsbiblioteket</a>
+                            <a href="https://www.ub.uio.no/om/personvernerkleringer/">Personvernerklæring</a>
+                            <div>Tjenesten er levert av BIBSYS.</div>
+                        </div>
+
+                        </p>
+                    </div>
+                </div>
+
+                <div flex="0" flex-md="10" flex-lg="25" ng-class="{'flex-lgPlus-30': $ctrl.parentCtrl.mediaQueries.lgPlus && !$ctrl.parentCtrl.facetToLeft, 'flex-lgPlus-25': $ctrl.parentCtrl.mediaQueries.lgPlus && $ctrl.parentCtrl.facetToLeft, 'flex-xl-30': $ctrl.parentCtrl.mediaQueries.xl && !$ctrl.parentCtrl.facetToLeft, 'flex-xl-25': $ctrl.parentCtrl.mediaQueries && $ctrl.parentCtrl.facetToLeft}"></div>
+
+            </div>`
+    });
+
+    app.controller('prmSearchAfterController', [function () {
         var vm = this;
-        vm.getIconLink = getIconLink;
-        function getIconLink() {
-            return vm.parentCtrl.iconLink;
-        }
-    }]);	
+
+        angular.element(document).ready(function () {
+            var footerSpacing = document.querySelector('.footer-spacing');
+            if (footerSpacing) {
+                angular.element(document.querySelector('.footer')).addClass('visible');
+            }
+        });
+    }]);
 
 })();
