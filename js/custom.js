uio-library/primo-explore-uio

commit 82ca70c60b3d4196a2620cd38193a6dc9a42aa1a

Move footer content to the HTML templates

css/custom1.css
@@ -264,62 +264,85 @@ prm-logo .logo-image, prm-logo img {
 	display:none;
 }*/
 
-.footer {
+
+/******************************************************************************
+ BEGIN Footer
+ *****************************************************************************/
+
+.uio-footer {
 	display: none;
 	background-color: #1B1B1B;
 	color: #C9C9C9;
 	width: 100%;
 }
-.footer.visible {
+.uio-footer.visible {
 	display: block;
 }
-.footer .uio-title {
+.uio-footer-main {
+	display: flex;
+	padding: 16px 20px;
+}
+.uio-footer-column {
+	flex: 0 0 auto;
+	padding: 0 1em;
+}
+.uio-footer .uio-title {
 	color: #FFFFFF;
 	font-size: 16px;
 }
-.logo img {
+
+.uio-footer .uio-logo {
+	flex: 0 0 auto;
+	display: block;
+	padding: 0;
+	margin-right: 1em;
+}
+.uio-footer .uio-logo img {
 	display: block;
 	margin: 0;
 	padding: 0;
 	width:50px;
 	height:50px;
 }
-.footer a {
+.uio-footer .line {
 	display: block;
+}
+.uio-footer a {
 	color: #FFFFFF;
 }
-.footer a:active {
+.uio-footer a:hover {
 	color: #EEEEEE;
+	text-decoration: underline;
 }
-.footer .uio-mobile {
+.uio-mobile {
 	display: none;
 }
-
 @media (max-width: 35em) {
-	.footer .md-headline {
+	.uio-footer .md-headline {
 		font-size: 18px;
 	}
-	.footer .uio-mobile {
-		display: block;
-	}
-	.footer .uio-contact, .footer .uio-responsible {
+	.uio-footer-column {
 		display: none;
+	}
+	.uio-mobile {
+		display: block;
 	}
 }
 
 @media (min-width: 35em) {
-	.footer {
+	.uio-footer {
 		bottom:0;
 		position:fixed;
 	}
-	.footer .uio-title {
-		font-size: 30px;
-	}
-	.footer-spacing {
+	.uio-footer-spacing {
 		height:150px;
 	}
-	.logo img {
+	.uio-footer .uio-logo img {
 		width:102px;
 		height:102px;
 	}
 }
+
+/******************************************************************************
+ END Footer
+ *****************************************************************************/

css/ubo.css
@@ -264,62 +264,85 @@ prm-logo .logo-image, prm-logo img {
 	display:none;
 }*/
 
-.footer {
+
+/******************************************************************************
+ BEGIN Footer
+ *****************************************************************************/
+
+.uio-footer {
 	display: none;
 	background-color: #1B1B1B;
 	color: #C9C9C9;
 	width: 100%;
 }
-.footer.visible {
+.uio-footer.visible {
 	display: block;
 }
-.footer .uio-title {
+.uio-footer-main {
+	display: flex;
+	padding: 16px 20px;
+}
+.uio-footer-column {
+	flex: 0 0 auto;
+	padding: 0 1em;
+}
+.uio-footer .uio-title {
 	color: #FFFFFF;
 	font-size: 16px;
 }
-.logo img {
+
+.uio-footer .uio-logo {
+	flex: 0 0 auto;
+	display: block;
+	padding: 0;
+	margin-right: 1em;
+}
+.uio-footer .uio-logo img {
 	display: block;
 	margin: 0;
 	padding: 0;
 	width:50px;
 	height:50px;
 }
-.footer a {
+.uio-footer .line {
 	display: block;
+}
+.uio-footer a {
 	color: #FFFFFF;
 }
-.footer a:active {
+.uio-footer a:hover {
 	color: #EEEEEE;
+	text-decoration: underline;
 }
-.footer .uio-mobile {
+.uio-mobile {
 	display: none;
 }
-
 @media (max-width: 35em) {
-	.footer .md-headline {
+	.uio-footer .md-headline {
 		font-size: 18px;
 	}
-	.footer .uio-mobile {
-		display: block;
-	}
-	.footer .uio-contact, .footer .uio-responsible {
+	.uio-footer-column {
 		display: none;
+	}
+	.uio-mobile {
+		display: block;
 	}
 }
 
 @media (min-width: 35em) {
-	.footer {
+	.uio-footer {
 		bottom:0;
 		position:fixed;
 	}
-	.footer .uio-title {
-		font-size: 30px;
-	}
-	.footer-spacing {
+	.uio-footer-spacing {
 		height:150px;
 	}
-	.logo img {
+	.uio-footer .uio-logo img {
 		width:102px;
 		height:102px;
 	}
 }
+
+/******************************************************************************
+ END Footer
+ *****************************************************************************/

js/custom.js
@@ -17,58 +17,21 @@
     app.component('prmSearchAfter', {
         bindings: {parentCtrl: '<'},
         controller: 'prmSearchAfterController',
-        template: `<div class="footer">
-
-            <div layout="row" layout-align="center start" flex>
-
-                <div flex="0" flex-md="0" flex-lg="10" flex-xl="20" ng-class="{'flex-lgPlus-15': $ctrl.parentCtrl.mediaQueries.lgPlus && !$ctrl.parentCtrl.facetToLeft, 'flex-lgPlus-20': $ctrl.parentCtrl.mediaQueries.lgPlus && $ctrl.parentCtrl.facetToLeft, 'flex-xl-25': $ctrl.parentCtrl.facetToLeft}"></div>
-
-                <div flex style="padding: 16px 20px; display: flex;">
-
-                    <div style="flex: 0 0 auto;" class="logo">
-                        <a href="https://www.ub.uio.no" title="Universitetsbiblioteket i Oslo" class="md-primoExplore-theme" style="display: block; margin-right: 1em; padding: 0;">
-                            <img src="custom/UBO/img/UiO_Segl.png" alt=""></a>
-                    </div>
-                    <div style="flex: 1 0 auto;">
-                        <!-- spacing -->
-                    </div>
-                    <div style="flex: 0 0 auto; padding: 0 1em;" class="uio-mobile">
-                        <div>
-                            <a href="#">oria-ub@ub.uio.no</a>
-                            <a href="https://www.ub.uio.no/om/personvernerkleringer/">Personvernerklæring</a>
-                        </div>
-                    </div>
-                    <div style="flex: 0 0 auto; padding: 0 1em;" class="uio-contact">
-                        <div class="md-headline">Kontaktinformasjon</div>
-                        <div>
-                            <a href="#">oria-ub@ub.uio.no</a>
-                            <a href="#">Kontakt oss</a>
-                        </div>
-                    </div>
-                    <div style="flex: 0 0 auto; padding: 0 1em;" class="uio-responsible">
-                        <div class="md-headline">Ansvarlig for denne siden</div>
-                        <div>
-                            <a href="#">UiO : Universitetsbiblioteket</a>
-                            <a href="https://www.ub.uio.no/om/personvernerkleringer/">Personvernerklæring</a>
-                            <div>Tjenesten er levert av BIBSYS.</div>
-                        </div>
-
-                        </p>
-                    </div>
-                </div>
-
-                <div flex="0" flex-md="10" flex-lg="25" ng-class="{'flex-lgPlus-30': $ctrl.parentCtrl.mediaQueries.lgPlus && !$ctrl.parentCtrl.facetToLeft, 'flex-lgPlus-25': $ctrl.parentCtrl.mediaQueries.lgPlus && $ctrl.parentCtrl.facetToLeft, 'flex-xl-30': $ctrl.parentCtrl.mediaQueries.xl && !$ctrl.parentCtrl.facetToLeft, 'flex-xl-25': $ctrl.parentCtrl.mediaQueries && $ctrl.parentCtrl.facetToLeft}"></div>
-
-            </div>`
+        template: ''
     });
 
     app.controller('prmSearchAfterController', [function () {
         var vm = this;
 
         angular.element(document).ready(function () {
-            var footerSpacing = document.querySelector('.footer-spacing');
-            if (footerSpacing) {
-                angular.element(document.querySelector('.footer')).addClass('visible');
+
+            var footer = angular.element(document.querySelector('.uio-footer')),
+                footerSpacing = angular.element(document.querySelector('.uio-footer-spacing')),
+                prmSearchAfterEl = angular.element(document.querySelector('prm-search-after'));
+
+            if (footer) {
+                // We are on the front page. Move footer and make it visible
+                prmSearchAfterEl.append(footer.detach().addClass('visible'));
             }
         });
     }]);

html/home_en_US.html
@@ -54,3 +54,58 @@
         </md-card>
     </div>
 </md-content>
+
+<div class="uio-footer-spacing"></div>
+<div class="uio-footer">
+
+    <div layout="row" layout-align="center start" flex>
+
+        <!-- Spacing til venstre -->
+        <div flex="0" flex-md="0" flex-lg="10" flex-xl="20" ng-class="{'flex-lgPlus-15': $ctrl.parentCtrl.mediaQueries.lgPlus && !$ctrl.parentCtrl.facetToLeft, 'flex-lgPlus-20': $ctrl.parentCtrl.mediaQueries.lgPlus && $ctrl.parentCtrl.facetToLeft, 'flex-xl-25': $ctrl.parentCtrl.facetToLeft}"></div>
+
+        <!-- Hovedinnhold -->
+        <div flex class="uio-footer-main">
+
+            <!-- Segl -->
+            <div class="uio-logo">
+                <a href="https://www.ub.uio.no" title="Universitetsbiblioteket i Oslo"><img src="custom/UBO/img/UiO_Segl.png" alt=""></a>
+            </div>
+
+            <!-- Spacing -->
+            <div style="flex: 1 0 auto;">
+            </div>
+
+            <!-- Column shown on small screens -->
+            <div class="uio-footer-column uio-mobile">
+                <div>
+                    <a class="line" href="mailto:oria-ub@ub.uio.no">oria-ub@ub.uio.no</a>
+                    <a class="line" href="https://www.ub.uio.no/om/personvernerkleringer/">Privacy policy</a>
+                </div>
+            </div>
+
+            <!-- Column shown on large screens -->
+            <div class="uio-footer-column">
+                <div class="md-headline">Conctact information</div>
+                <div>
+                    <a class="line" href="mailto:oria-ub@ub.uio.no">oria-ub@ub.uio.no</a>
+                    <a class="line" href="https://nettskjema.uio.no/answer/61878.html">Contact us</a>
+                </div>
+            </div>
+
+            <!-- Column shown on large screens -->
+            <div class="uio-footer-column">
+                <div class="md-headline">Responsible for this page</div>
+                <div>
+                    <a class="line" href="https://ub.uio.no/english/">UiO : University of Oslo Library</a>
+                    <a class="line" href="https://www.ub.uio.no/om/personvernerkleringer/">Privacy policy</a>
+                    <div>The service is provided by <a href="https://www.bibsys.no">BIBSYS</a>.</div>
+                </div>
+            </div>
+
+        </div>
+
+        <!-- Spacing til høyre-->
+        <div flex="0" flex-md="10" flex-lg="25" ng-class="{'flex-lgPlus-30': $ctrl.parentCtrl.mediaQueries.lgPlus && !$ctrl.parentCtrl.facetToLeft, 'flex-lgPlus-25': $ctrl.parentCtrl.mediaQueries.lgPlus && $ctrl.parentCtrl.facetToLeft, 'flex-xl-30': $ctrl.parentCtrl.mediaQueries.xl && !$ctrl.parentCtrl.facetToLeft, 'flex-xl-25': $ctrl.parentCtrl.mediaQueries && $ctrl.parentCtrl.facetToLeft}"></div>
+
+    </div>
+</div>

html/home_nn_NO.html
@@ -65,3 +65,57 @@
     </div>
 </md-content>
 
+<div class="uio-footer-spacing"></div>
+<div class="uio-footer">
+
+    <div layout="row" layout-align="center start" flex>
+
+        <!-- Spacing til venstre -->
+        <div flex="0" flex-md="0" flex-lg="10" flex-xl="20" ng-class="{'flex-lgPlus-15': $ctrl.parentCtrl.mediaQueries.lgPlus && !$ctrl.parentCtrl.facetToLeft, 'flex-lgPlus-20': $ctrl.parentCtrl.mediaQueries.lgPlus && $ctrl.parentCtrl.facetToLeft, 'flex-xl-25': $ctrl.parentCtrl.facetToLeft}"></div>
+
+        <!-- Hovedinnhold -->
+        <div flex class="uio-footer-main">
+
+            <!-- Segl -->
+            <div class="uio-logo">
+                <a href="https://www.ub.uio.no" title="Universitetsbiblioteket i Oslo"><img src="custom/UBO/img/UiO_Segl.png" alt=""></a>
+            </div>
+
+            <!-- Spacing -->
+            <div style="flex: 1 0 auto;">
+            </div>
+
+            <!-- Column shown on small screens -->
+            <div class="uio-footer-column uio-mobile">
+                <div>
+                    <a href="mailto:oria-ub@ub.uio.no">oria-ub@ub.uio.no</a>
+                    <a href="https://www.ub.uio.no/om/personvernerkleringer/">Personvernerklæring</a>
+                </div>
+            </div>
+
+            <!-- Column shown on large screens -->
+            <div class="uio-footer-column">
+                <div class="md-headline">Kontaktinformasjon</div>
+                <div>
+                    <a href="mailto:oria-ub@ub.uio.no">oria-ub@ub.uio.no</a>
+                    <a href="https://nettskjema.uio.no/answer/61878.html">Kontakt oss</a>
+                </div>
+            </div>
+
+            <!-- Column shown on large screens -->
+            <div class="uio-footer-column">
+                <div class="md-headline">Ansvarleg for denne sida</div>
+                <div>
+                    <a href="https://ub.uio.no/">UiO : Universitetsbiblioteket</a>
+                    <a href="https://www.ub.uio.no/om/personvernerkleringer/">Personvernerklæring</a>
+                    <div>Tenesta er levert av <a href="https://www.bibsys.no">BIBSYS</a>.</div>
+                </div>
+            </div>
+
+        </div>
+
+        <!-- Spacing til høyre-->
+        <div flex="0" flex-md="10" flex-lg="25" ng-class="{'flex-lgPlus-30': $ctrl.parentCtrl.mediaQueries.lgPlus && !$ctrl.parentCtrl.facetToLeft, 'flex-lgPlus-25': $ctrl.parentCtrl.mediaQueries.lgPlus && $ctrl.parentCtrl.facetToLeft, 'flex-xl-30': $ctrl.parentCtrl.mediaQueries.xl && !$ctrl.parentCtrl.facetToLeft, 'flex-xl-25': $ctrl.parentCtrl.mediaQueries && $ctrl.parentCtrl.facetToLeft}"></div>
+
+    </div>
+</div>

html/home_no_NO.html
@@ -54,6 +54,57 @@
     </div>
 </md-content>
 
-<div class="footer-spacing"></div>
+<div class="uio-footer-spacing"></div>
+<div class="uio-footer">
 
+    <div layout="row" layout-align="center start" flex>
+
+        <!-- Spacing til venstre -->
+        <div flex="0" flex-md="0" flex-lg="10" flex-xl="20" ng-class="{'flex-lgPlus-15': $ctrl.parentCtrl.mediaQueries.lgPlus && !$ctrl.parentCtrl.facetToLeft, 'flex-lgPlus-20': $ctrl.parentCtrl.mediaQueries.lgPlus && $ctrl.parentCtrl.facetToLeft, 'flex-xl-25': $ctrl.parentCtrl.facetToLeft}"></div>
+
+        <!-- Hovedinnhold -->
+        <div flex class="uio-footer-main">
+
+            <!-- Segl -->
+            <div class="uio-logo">
+                <a href="https://www.ub.uio.no" title="Universitetsbiblioteket i Oslo"><img src="custom/UBO/img/UiO_Segl.png" alt=""></a>
+            </div>
+
+            <!-- Spacing -->
+            <div style="flex: 1 0 auto;">
+            </div>
+
+            <!-- Column shown on small screens -->
+            <div class="uio-footer-column uio-mobile">
+                <div>
+                    <a href="mailto:oria-ub@ub.uio.no">oria-ub@ub.uio.no</a>
+                    <a href="https://www.ub.uio.no/om/personvernerkleringer/">Personvernerklæring</a>
+                </div>
+            </div>
+
+            <!-- Column shown on large screens -->
+            <div class="uio-footer-column">
+                <div class="md-headline">Kontaktinformasjon</div>
+                <div>
+                    <a href="mailto:oria-ub@ub.uio.no">oria-ub@ub.uio.no</a>
+                    <a href="https://nettskjema.uio.no/answer/61878.html">Kontakt oss</a>
+                </div>
+            </div>
+
+            <!-- Column shown on large screens -->
+            <div class="uio-footer-column">
+                <div class="md-headline">Ansvarlig for denne siden</div>
+                <div>
+                    <a href="https://ub.uio.no/">UiO : Universitetsbiblioteket</a>
+                    <a href="https://www.ub.uio.no/om/personvernerkleringer/">Personvernerklæring</a>
+                    <div>Tjenesten er levert av <a href="https://www.bibsys.no">BIBSYS</a>.</div>
+                </div>
+            </div>
+
+        </div>
+
+        <!-- Spacing til høyre-->
+        <div flex="0" flex-md="10" flex-lg="25" ng-class="{'flex-lgPlus-30': $ctrl.parentCtrl.mediaQueries.lgPlus && !$ctrl.parentCtrl.facetToLeft, 'flex-lgPlus-25': $ctrl.parentCtrl.mediaQueries.lgPlus && $ctrl.parentCtrl.facetToLeft, 'flex-xl-30': $ctrl.parentCtrl.mediaQueries.xl && !$ctrl.parentCtrl.facetToLeft, 'flex-xl-25': $ctrl.parentCtrl.mediaQueries && $ctrl.parentCtrl.facetToLeft}"></div>
+
+    </div>
 </div>

html/home_se_NO.html
@@ -106,3 +106,6 @@
         </md-card>
 		</div>
 </md-content>
+
+<div class="footer-spacing"></div>
+

commit 086b92bef081f7e9c661106620c0291f19b09653

Fix footer scope (broken since 82ca70c)

css/custom1.css
@@ -280,7 +280,7 @@ prm-logo .logo-image, prm-logo img {
 }
 .uio-footer-main {
 	display: flex;
-	padding: 16px 20px;
+	padding: 16px 24px;
 }
 .uio-footer-column {
 	flex: 0 0 auto;

css/ubo.css
@@ -280,7 +280,7 @@ prm-logo .logo-image, prm-logo img {
 }
 .uio-footer-main {
 	display: flex;
-	padding: 16px 20px;
+	padding: 16px 24px;
 }
 .uio-footer-column {
 	flex: 0 0 auto;

js/custom.js
@@ -20,11 +20,8 @@
         template: ''
     });
 
-    app.controller('prmSearchAfterController', [function () {
-        var vm = this;
-
+    app.controller('prmSearchAfterController', ['$scope', '$compile', function ($scope, $compile) {
         angular.element(document).ready(function () {
-
             var footer = angular.element(document.querySelector('.uio-footer')),
                 footerSpacing = angular.element(document.querySelector('.uio-footer-spacing')),
                 prmSearchAfterEl = angular.element(document.querySelector('prm-search-after'));
@@ -32,6 +29,8 @@
             if (footer) {
                 // We are on the front page. Move footer and make it visible
                 prmSearchAfterEl.append(footer.detach().addClass('visible'));
+                var fnLink = $compile(footer);      // returns a Link function used to bind template to the scope
+                fnLink($scope);                     // Bind scope to the template
             }
         });
     }]);