f40c722c9ac741bc50e267a9e6d5113a16207178
Dan Michael O. Heggø <d.m.heggo@ub.uio.no>
2017-12-16 20:05:04 +0100

    Footer: Add small timeout

--- js/custom.js
+++ js/custom.js
@@ -20,18 +20,23 @@
         template: ''
     });
 
-    app.controller('prmSearchAfterController', ['$scope', '$compile', function ($scope, $compile) {
-        angular.element(document).ready(function () {
-            var footer = angular.element(document.querySelector('.uio-footer')),
-                footerSpacing = angular.element(document.querySelector('.uio-footer-spacing')),
-                prmSearchAfterEl = angular.element(document.querySelector('prm-search-after'));
+    app.controller('prmSearchAfterController', ['$scope', '$compile', '$timeout', '$document', function ($scope, $compile, $timeout, $document) {
+        $document.ready(function () {
+            // Note: At this point, the frontpage HTML template might not yet be ready.
+            // We see this problem especially in Firefox for some reason. Until we find a better
+            // way to detect when the template is loaded, we use a timeout of 100 msecs.
+            $timeout(function moveFooter() {
+                var footer = angular.element(document.querySelector('.uio-footer')),
+                    footerSpacing = angular.element(document.querySelector('.uio-footer-spacing')),
+                    prmSearchAfterEl = angular.element(document.querySelector('prm-search-after'));
 
-            if (footer) {
-                // We are on the front page. Move footer and make it visible
-                prmSearchAfterEl.append(footer.detach().addClass('visible'));
-                var fnLink = $compile(footer);      // returns a Link function used to bind template to the scope
-                fnLink($scope);                     // Bind scope to the template
-            }
+                if (footer.length) {
+                    // We are on the front page. Move footer and make it visible
+                    prmSearchAfterEl.append(footer.detach().addClass('visible'));
+                    var fnLink = $compile(footer);      // returns a Link function used to bind template to the scope
+                    fnLink($scope);                     // Bind scope to the template
+                }
+            }, 100);
         });
     }]);
 
