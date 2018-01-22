741653ada236f20ade3b3c7cacb13231faeca58d
Dan Michael O. Heggø <danmichaelo@gmail.com>
2018-01-22 01:47:51 +0100

    Slurp: Fix tracking of browse search results
    Since the browse search result list pages don't use the SearchBar component, we grab searchStateService from SearchResultList instead

--- js/prmSearchResultListAfter.component.js
+++ js/prmSearchResultListAfter.component.js
@@ -1,7 +1,16 @@
+import get from 'lodash/get';
 
 class PrmSearchResultListAfterController {
 
     constructor($window, $scope, loggingService) {
+
+        let primoVersion = get($window.appConfig, 'system-configuration.Primo_Version_Number', 'unknown');
+        let searchStateService = this.parentCtrl.searchService.searchStateService;
+
+        // Inject Primo's searchStateService into our loggingService
+        loggingService.setSearchStateService(searchStateService);
+        loggingService.setPrimoVersion(primoVersion);
+
         $scope.$watch('$ctrl.parentCtrl.numOfLoadedPages', (newValue) => {
             if (newValue) {
                 loggingService.searchPageLoaded(newValue);
