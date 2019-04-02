321764d9a9d4b73b931424ea833d701394063b32
Dan Michael O. Heggø <danmichaelo@gmail.com>
2019-04-02 18:26:07 +0200

    Cleanup unused JS code

--- js/main.js
+++ js/main.js
@@ -4,12 +4,3 @@ const app = angular.module('viewCustom', ['angularLoad']);
 
 // SearchAfter: Everything below the searchbar. Reloaded on normal page changes
 app.component('prmSearchAfter', prmSearchAfterComponent);
-
-// ------------------------------------------------------------------------
-
-// eslint-disable-next-line no-unused-vars
-app.run(['$rootScope', 'loggingService', ($rootScope, loggingService) => {
-    // WARNING: This might not be called if Primo errors..
-    // Components may still be initialized
-    $rootScope.viewName = viewName;
-}]);
